921f0df576fa928548b891e27406eb4b0e9bc82f
jornbos <113889452+jornbos@users.noreply.github.com>
2022-11-08 13:15:50 +0100

    Module2 bijgewerkt
    comments toegevoegd en nu echt werkend gekregen.

--- modules/Module 2.py
+++ modules/Module 2.py
@@ -1,12 +1,11 @@
 from datetime import datetime
 import psycopg2
+import os
 
 tijd = str(datetime.now())
 tijd = tijd[0:19]
 
-modmail = "JORN.BOS@STUDENT.HU.NL"
-modnaam = "JORN BOS"
-
+aantal = 0
 
 #verbinden met DB
 con = psycopg2.connect( 
@@ -23,27 +22,40 @@ cur = con.cursor()
 berichten = []
 bericht = []
 lijst_moderator = []
+
 #opent het bestand met alle resultaten
-r = open("resultaat.csv", "r")
+r = open("bericht.csv", "r")
 berichten = r.readlines()
 r.close
 
+#vraagt naar naam moderator
 modnaam = input("Wat is uw naam?: ")
+
+#als de naam niet gelijk is aan "jorn bos" stopt het programma
 if modnaam.lower() != "jorn bos":
       print(exit("Dit is geen geldige naam."))
+
+#vraagt naar naam moderator
 modmail = input("Wat is uw mail?: ")
+
+#als de naam niet gelijk is aan "jorn bos" stopt het programma
 if  modmail.lower() != "jorn.bos@student.hu.nl":
-    print(exit("Dit is geen geldige mailadres"))   
+    print(exit("Dit is geen geldige mailadres")) 
+
+#hiermee kijkt python of het bestand waar het om gaat leeg is of niet, als het leeg is word het programma met het onderstaande bericht afgesloten
+if os.path.getsize("bericht.csv") == 0:
+    print(exit("Er zijn geen berichten.\nProgramma word afgesloten"))  
 
 for x in berichten:
     bericht = x.strip("\n").split(";")
     print(f"\nNaam: {bericht[0]} \nBericht: {bericht[1]}\n")
     
+    #vraagt of het bericht is goedgekeurd
     goedkeuring = input("Keurt u dit bericht goed?(ja/nee): ")
     
     if goedkeuring.upper() == "JA":
         goedgekeurd = 1
-
+        
     if goedkeuring.upper() == "NEE":
         goedgekeurd = 0
     
@@ -54,11 +66,12 @@ for x in berichten:
     sqlcode = "INSERT INTO bericht (naam, bericht, datum_tijd, stationnaam, goedgekeurd_door, Mailadres, goedgekeurd_tijd) VALUES (%s,%s,%s,%s,%s,%s,%s)"
     if goedgekeurd == 1:
         cur.execute(sqlcode, bericht)
+        aantal = aantal + 1
+        print(f"Er zijn {aantal} goedgekeurde berichten toegevoegd aan de database.\nHet bericht.csv word nu geleegd!")
     
 con.commit()
 
-leeg = open("resultaat.csv", "w")
+
+leeg = open("bericht.csv", "w")
 leeg.truncate(0)
 leeg.close()
-
-
